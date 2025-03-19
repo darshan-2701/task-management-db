33251cf8cb8869d3d4c091e20ca81fdb8bc4c31d
darshan-2701 <133952219+darshan-2701@users.noreply.github.com>
2025-03-19 18:46:25 +0530

    changes

--- postgres.sql
+++ postgres.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS darshan;
 
 CREATE TABLE IF NOT EXISTS darshan.task_details (
     id SERIAL PRIMARY KEY,
-    title text,
-	  description text,
-	  status varchar(20)
+    title varchar(100),
+    description varchar(200),
+    status varchar(20)
 );
